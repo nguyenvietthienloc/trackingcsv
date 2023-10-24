afafccdbe8d6e166d55fa2884a87fa996add0384
nguyenvietthienloc <loc.nguyen@flexidata.vn>
2023-10-24 15:01:07 +0700

    feat: update-icon-logo-name-fxd

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with by <b>Loc Nguyen</b> 2023
+      Created with by <b>Flexidata</b> 2023
     </span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
